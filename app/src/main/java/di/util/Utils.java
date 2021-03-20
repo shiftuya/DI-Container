98e0fed9f6962173d5370101c9fab6f6a4d8abdc
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-03-20 11:24:58 +0700

    Fix getResourceAsString()

--- app/src/main/java/di/util/Utils.java
+++ app/src/main/java/di/util/Utils.java
@@ -1,12 +1,10 @@
 package di.util;
 
-import di.beanparser.JsonBeanParser;
-
 import java.io.IOException;
 
 public class Utils {
     public static String getResourceAsString(String resourceName) throws IOException {
-        return new String(JsonBeanParser.class.getResourceAsStream("/" + resourceName).readAllBytes());
+        return new String(Utils.class.getResourceAsStream("/" + resourceName).readAllBytes());
     }
 
     public static String capitalize(String str) {
