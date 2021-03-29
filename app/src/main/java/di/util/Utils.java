0a73850ea801723c08a5576097decfff2f4d0051
Simon Morozov <morozzmorozov@gmail.com>
2021-03-29 15:58:29 +0700

    Add splitByComma

--- app/src/main/java/di/util/Utils.java
+++ app/src/main/java/di/util/Utils.java
@@ -1,6 +1,8 @@
 package di.util;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Utils {
     public static String getResourceAsString(String resourceName) throws IOException {
@@ -12,5 +14,9 @@ public class Utils {
             return str;
         }
         return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    public static List<String> splitByComma(String str) {
+        return Arrays.asList(str.split(","));
     }
 }
